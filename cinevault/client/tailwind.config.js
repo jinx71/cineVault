bedf5c94469dab7a7fe2fcc062cbc54b8c5fcbc5
jinx71 <karim.sazed@gmail.com>
2021-04-06 00:33:23 +0000

    chore: configure Tailwind CSS for client

---
+++ cinevault/client/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    extend: {
+      colors: {
+        // CineVault accent: deep violet + gold on a near-black "vault" backdrop
+        brand: {
+          50: '#f5f3ff',
+          100: '#ede9fe',
+          200: '#ddd6fe',
+          300: '#c4b5fd',
+          400: '#a78bfa',
+          500: '#8b5cf6',
+          600: '#7c3aed',
+          700: '#6d28d9',
+          800: '#5b21b6',
+          900: '#4c1d95',
+        },
+        gold: {
+          400: '#fbbf24',
+          500: '#f59e0b',
+          600: '#d97706',
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+        display: ['Poppins', 'Inter', 'sans-serif'],
+      },
+      boxShadow: {
+        card: '0 6px 24px -6px rgba(76, 29, 149, 0.45)',
+      },
+    },
+  },
+  plugins: [],
+};
